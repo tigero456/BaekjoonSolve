31ca0b1511c7e535b9acc85daf5b52794a4dff1e
tigero456 <95518178+tigero456@users.noreply.github.com>
2024-06-18 11:44:28 +0900

    Add files via upload

---
+++ CH_Java/Baek_Level1_10926.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class B10926 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String Name = sc.nextLine();
+
+    System.out.println(Name + "??!");
+    
+   }
+}
---
+++ CH_Java/Baek_Level1_11382.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class B11382 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long A, B, C;
+
+        A = sc.nextLong();
+        B = sc.nextLong();
+        C = sc.nextLong();
+
+        System.out.println(A + B + C);
+
+    }
+}
