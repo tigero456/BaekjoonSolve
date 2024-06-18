5525107de24c1e5c804353a3bff4b59c11cb8b92
tigero456 <95518178+tigero456@users.noreply.github.com>
2024-06-18 11:30:03 +0900

    Add files via upload

---
+++ CH_Java/B18108.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class B18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int i = sc.nextInt();
+
+        System.out.println(i-543);
+    }
+}
